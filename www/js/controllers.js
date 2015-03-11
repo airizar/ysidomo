31d7b33b084b2b09e1e69618e7c5826831e70bb2
Javier Vaquero <javi.vaquero@gmail.com>
2015-03-11 19:28:52 +0100

    cambio notacion .controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', ["$scope",function($scope) {}])
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', ["$scope", "Chats", function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
-  }
-})
+  };
+}])
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('ChatDetailCtrl', ["$scope", "$stateParams", "Chats", function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
+}])
 
-.controller('FriendsCtrl', function($scope, Friends) {
+.controller('FriendsCtrl', ["$scope", "Friends" ,function($scope, Friends) {
   $scope.friends = Friends.all();
-})
+}])
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+.controller('FriendDetailCtrl', ["$scope", "$stateParams", "Friends", function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
-})
+}])
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', ["$scope",function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-});
+}]);
