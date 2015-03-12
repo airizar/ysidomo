1c897f50dcf5b8112fbfa0ce08eadc1568ad5462
Javier Vaquero <javi.vaquero@gmail.com>
2015-03-12 19:28:00 +0100

    cambios modulo DB y controller datos

--- www/js/bd.js
+++ www/js/bd.js
@@ -1,26 +1,82 @@
 angular.module('DB', [])
 .factory('Devices', function () {
+
+var db = new ydn.db.Storage('ysidomo');
+	db.put('devices', {
+		name: 'Luz',
+		room: 'Sala',
+		type: 'binario',
+		status: true}, 0);
+	db.put('devices', {
+		name: 'Persiana',
+		room: 'Sala',
+		type: 'decimal',
+		status: 10}, 1);
+	db.put('devices', {
+		name: 'Televisión',
+		room: 'Sala',
+		type: 'binario',
+		status: false}, 2);
+	db.put('devices', {
+		name: 'Luz',
+		room: 'Baño',
+		type: 'binario',
+		status: true}, 3);
+	db.put('devices', {
+		name: 'Persiana',
+		room: 'Baño',
+		type: 'decimal',
+		status: 10}, 4);
+
+	db.put('rooms', {
+		name: 'Sala'}, 0);
+	db.put('rooms', {
+		name: 'Baño'}, 1);
+	console.log(db);
+	
+
+
+
 	var devices=[{
 		id: 0,
 		name: 'Luz',
 		room: 'Sala',
 		type: 'binario',
-		status: 0
+		status: true
 	},{
 		id: 1,
-		name: 'Luz1',
+		name: 'Persiana',
 		room: 'Sala',
 		type: 'decimal',
 		status: 10
 	},{
 		id: 2,
-		name: 'Luz2',
+		name: 'Televisión',
 		room: 'Sala',
 		type: 'binario',
-		status: 1
+		status: false
+	},{
+		id: 5,
+		name: 'Luz',
+		room: 'Baño',
+		type: 'decimal',
+		status: true
+	},{
+		id: 6,
+		name: 'Persiana',
+		room: 'Baño',
+		type: 'decimal',
+		status: 10
 	}];
 
+	var getRooms = function(success){
+			db.from('rooms').list().done(function(records){
+					console.log(records);
+				});
+		};
+
 	return {
+		getRooms: getRooms,
 		getRoomDevices: function(room){
 			var device_list=[];
 			for (var i = devices.length - 1; i >= 0; i--) {
@@ -34,13 +90,19 @@ angular.module('DB', [])
 	};
 	
 
-})
+});/*
 .factory('Rooms', function () {
-		var rooms=['Sala','Baño'];
+
+
+		var getAll = function(success){
+			db.from('rooms').list().done(function(records){
+					console.log(records);
+				});
+		};
 	
 		return {
-			getAll : function(){
-				return rooms;
-			}
-		};
+			getAll : getAll
+				//return rooms;
+			};
 	});
+*/
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,50 +1,59 @@
 angular.module('starter.controllers', [])
 
 ////////////////////////////////
-.controller('DevicesCtrl', ["$scope","Rooms","Devices", "$stateParams", function($scope,Rooms,Devices, $stateParams) {
-  var rooms=Rooms.getAll();
-  var roomDevices=[];
-  for (var i = rooms.length - 1; i >= 0; i--) {
-    var devices=Devices.getRoomDevices(rooms[i]);
-    roomDevices.push({name:rooms[i],devices:devices});
-  }
-   $scope.devices=roomDevices;
-   
+.controller('DevicesCtrl', ["$scope", /*"Rooms",*/ "Devices", "$stateParams", function($scope, /*Rooms,*/ Devices, $stateParams) {
+    var rooms;
+    var success = function(rooms) {
+        var roomDevices = [];
+        for (var i = rooms.length - 1; i >= 0; i--) {
+          console.log(rooms[i]);
+          console.log(rooms[i].name);
+            var devices = Devices.getRoomDevices(rooms[i].name);
+            roomDevices.push({
+                name: rooms[i],
+                devices: devices
+            });
+        }
+        $scope.devices = roomDevices;
+    };
+    Devices.getRooms(success);
+
+
 
 }])
 
-.controller('WarningsCtrl', ["$scope" ,function($scope) {
-  //$scope.friends = Friends.all();
+.controller('WarningsCtrl', ["$scope", function($scope) {
+    //$scope.friends = Friends.all();
 }])
 
 .controller('ActionsCtrl', ["$scope", "$stateParams", function($scope, $stateParams) {
- // $scope.friend = Friends.get($stateParams.friendId);
-}])
-///////////////////////////////
+        // $scope.friend = Friends.get($stateParams.friendId);
+    }])
+    ///////////////////////////////
 
-.controller('DashCtrl', ["$scope",function($scope) {}])
+.controller('DashCtrl', ["$scope", function($scope) {}])
 
 .controller('ChatsCtrl', ["$scope", "Chats", function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
 }])
 
 .controller('ChatDetailCtrl', ["$scope", "$stateParams", "Chats", function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 }])
 
-.controller('FriendsCtrl', ["$scope", "Friends" ,function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', ["$scope", "Friends", function($scope, Friends) {
+    $scope.friends = Friends.all();
 }])
 
 .controller('FriendDetailCtrl', ["$scope", "$stateParams", "Friends", function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+    $scope.friend = Friends.get($stateParams.friendId);
 }])
 
-.controller('AccountCtrl', ["$scope",function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', ["$scope", function($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 }]);
